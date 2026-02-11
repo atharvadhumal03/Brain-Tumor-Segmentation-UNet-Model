c738c77c4fda9150d9f80c78d7f86d21bd9e520c
Atharva Dhumal <atharvapdhumal@gmail.com>
2026-02-11 10:24:29 -0800

    Add PyTorch Dataset class for brain MRI data

---
+++ src/dataset.py
@@ -0,0 +1,54 @@
+import os
+import cv2
+import numpy as np
+from torch.utils.data import Dataset
+from glob import glob
+
+
+class BrainMRIDataset(Dataset):
+    def __init__(self, data_path, transform=None):
+        """
+        Args:
+            data_path: Path to the kaggle_3m folder
+            transform: Optional transforms to apply
+        """
+        self.data_path = data_path
+        self.transform = transform
+        
+        # Get all patient folders
+        patient_folders = sorted(glob(os.path.join(data_path, 'TCGA*')))
+        
+        # Lists to collect all image-mask pairs
+        self.image_paths = []
+        self.mask_paths = []
+        
+        for patient_folder in patient_folders:
+            images = sorted(glob(os.path.join(patient_folder, '*[0-9].tif')))
+            for img_path in images:
+                mask_path = img_path.replace('.tif', '_mask.tif')
+                self.image_paths.append(img_path)
+                self.mask_paths.append(mask_path)
+        
+        print(f"Found {len(self.image_paths)} image-mask pairs")
+    
+    def __len__(self):
+        return len(self.image_paths)
+    
+    def __getitem__(self, idx):
+        # Load image and mask
+        image = cv2.imread(self.image_paths[idx])
+        mask = cv2.imread(self.mask_paths[idx], cv2.IMREAD_GRAYSCALE)
+        
+        # Convert BGR to RGB
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        # Normalize mask to 0 and 1
+        mask = mask / 255.0
+        
+        # Apply transforms if any
+        if self.transform:
+            transformed = self.transform(image=image, mask=mask)
+            image = transformed['image']
+            mask = transformed['mask']
+        
+        return image, mask
